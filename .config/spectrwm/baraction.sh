d6cfc706583b308298de4e890f9550df4b6d9e60
jc9361 <jc9361@gmail.com>
2020-12-21 02:51:17 -0800

    updating dotfiles

--- .config/spectrwm/baraction.sh
+++ .config/spectrwm/baraction.sh
@@ -9,19 +9,26 @@ COUNT=0
 
 # MAIN LOOP
 while :; do
-    # battery=$( acpi -V | head -n 1 | awk '{print $3, $4}'| sed 's/,//g')
 
+    # BATTERY
     if [[ $(acpi -b | awk 'NR==1 {print $4}'| sed 's/,//g') == 0% ]]; then 
-        battery=$(acpi -b | awk 'NR==2 {print $3, $4}' | sed 's/,//g') 
-    else battery=$(acpi -b | awk 'NR==1 {print $3, $4}' | sed 's/,//g')
+        battery=$(acpi -b | awk 'NR==2 {print $3, $4}' | sed 's/,//g' | sed 's/Unknown/Full/g') 
+    else battery=$(acpi -b | awk 'NR==1 {print $3, $4}' | sed 's/,//g' | sed 's/Unknown/Full/g')
     fi
-    
+
+    # MEMORY
     memory=$(free -mh | awk 'FNR == 2 {print}' | awk '{print $3}')
+
+    # WIFI NETWORK
     ssid=$(python3 ~/.config/spectrwm/bar/get_ssid.py)
+
+    # LOCAL IP
     local_ip=$(python3 ~/.config/spectrwm/bar/get_ip.py)
 
 	let COUNT=$COUNT+1
 
+
+        # DISPLAY BAR
         echo " [$ssid]   [$local_ip]    [$battery]"
         sleep $SLEEP_SEC
 done
