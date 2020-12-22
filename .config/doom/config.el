2d87de94b370ef8ecaec26aae490d00617429bea
jc9361 <jc9361@gmail.com>
2020-12-22 00:30:47 -0800

    adding doom emacs and rofi configs.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -4,19 +4,17 @@
 ;;|  __/ | | | | | (_| | (__\__ \ | (_| (_) | | | |  _| | (_| |
 ;; \___|_| |_| |_|\__,_|\___|___/  \___\___/|_| |_|_| |_|\__, |
 ;;                                                       |___/
+;;
+;;
 ;; Build: ThinkPad X200
 ;; Version 1.0
 
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
-
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
 
 
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "R3DUX"
+      user-mail-address "jc9361@gmail.com")
+
+
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -27,22 +25,30 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
+
  (setq doom-font (font-spec :family "Hack Nerd Font" :size 12 :weight 'regular)
-       doom-variable-pitch-font (font-spec :family "sans" :size 12))
+       doom-variable-pitch-font (font-spec :family "sans" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
+
 (setq doom-theme 'xresources)
+
 
 (add-to-list 'default-frame-alist '(background-color . "#101216"))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
+
 (setq org-directory "~/docs/org/")
+(setq org-agenda-files "~/docs/org/agenda.org")
 
-(setq org-agenda-files "~/docs/org/")
 
+;; ORG BULLETS
+(after! org
+  (require 'org-bullets)  ; Nicer bullets in org-mode
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -70,3 +76,6 @@
 ;; DEFAULT BROWSER FOR EMACS
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "surf")
+
+
+
